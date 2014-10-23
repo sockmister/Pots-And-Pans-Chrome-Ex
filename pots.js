c278243ff162910da1fe3c21a6464ce77b8aff6d
unknown <pohchiat.koh@P29562.intra.rakuten.co.jp>
2014-10-23 18:26:04 +0900

    add code in pots.js to extract steps add parser.js

--- pots.js
+++ pots.js
@@ -1,1 +1,24 @@
 alert("recipe page visited!");
+
+
+howToStepImg = document.getElementsByClassName("howtoStep")[0];
+rows = howToStepImg.children[0];
+numberOfRows = rows.childElementCount;
+
+for (i=0; i < numberOfRows; i++){
+
+  numberOfSteps = rows.children[i].childElementCount;
+  currRow = rows.children[i];
+
+  for (j=0; j < numberOfSteps; j++){
+    currStep = currRow.children[j];
+
+    // look for the first <p> tag
+    for(k=0; k < currStep.childElementCount; k++){
+      if(currStep.children[k].tagName == "P"){
+        comments = currStep.children[k];
+      }
+    }
+
+  }
+}
